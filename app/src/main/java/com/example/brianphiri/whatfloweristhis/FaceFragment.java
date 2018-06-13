8a8bb6ed06903d006616e82d0ae2cce12751006a
BrianPhiri <brianphiri.9523@gmail.com>
2018-06-13 16:37:33 +0300

    trigger camera and display captured image in imave view

--- app/src/main/java/com/example/brianphiri/whatfloweristhis/FaceFragment.java
+++ app/src/main/java/com/example/brianphiri/whatfloweristhis/FaceFragment.java
@@ -1,16 +1,58 @@
 package com.example.brianphiri.whatfloweristhis;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FaceFragment extends Fragment {
+    @BindView(R.id.ib_faceFragment)
+    ImageButton camera;
+    @BindView(R.id.iv_faceFragment)
+    ImageView imageView;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_face, container, false);
+        ButterKnife.bind(this, view);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private void openCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
 }
