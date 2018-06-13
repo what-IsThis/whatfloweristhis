0b6204f5e54ab312d00e7b36c89aa410a41f193e
BrianPhiri <brianphiri.9523@gmail.com>
2018-06-13 16:56:11 +0300

    call to the classification functions

--- app/src/main/java/com/example/brianphiri/whatfloweristhis/FaceFragment.java
+++ app/src/main/java/com/example/brianphiri/whatfloweristhis/FaceFragment.java
@@ -6,27 +6,48 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.brianphiri.whatfloweristhis.utils.Classifier;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static android.support.constraint.Constraints.TAG;
+
 public class FaceFragment extends Fragment {
+
+    private Classifier classifier;
+
     @BindView(R.id.ib_faceFragment)
     ImageButton camera;
     @BindView(R.id.iv_faceFragment)
     ImageView imageView;
-
+    @BindView(R.id.tv_faceFragment_title)
+    TextView title;
+    @BindView(R.id.tv_faceFragment_description)
+    TextView description;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_face, container, false);
         ButterKnife.bind(this, view);
+
+        try {
+            classifier = new Classifier(getActivity());
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to initialize an image classifier.");
+            Log.e(TAG, e.getMessage());
+        }
 
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +69,12 @@ public class FaceFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap) data.getExtras().get("data");
         imageView.setImageBitmap(bitmap);
+        if(classifier == null || getActivity() == null) {
+            Log.e(TAG, "Uninitialized Classifier or invalid context.");
+            return;
+        }
+        String pred = classifier.classify(bitmap);
+        description.setText(pred);
     }
 
     private void openCamera(){
